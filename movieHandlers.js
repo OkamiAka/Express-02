ac6ccf87523acbb5e80eb92b046e26bc5112f424
OkamiAka <nils.caqueux@gmail.com>
2023-04-25 11:53:23 +0200

    all

---
+++ movieHandlers.js
@@ -0,0 +1,36 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query(`select * from users where id=${id}`)
+    .then(([user]) => {
+      if (user.length > 0) {
+        res.status(200).json(user);
+      } else {
+        res.send(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
